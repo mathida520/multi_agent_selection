944221fbdd5dd5ca021b712bcb7fc86907c399e8
Ron Yeung <wwwinglung@gmail.com>
2024-09-04 20:42:00 +0800

    Update ChatPage.js

---
+++ frontend/src/pages/ChatPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatBot from '../components/ChatInterface.js';
+
+function ChatPage() {
+    return (
+        <div>
+            <ChatBot/>
+        </div>
+    );
+}
+
+export default ChatPage;
