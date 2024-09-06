f7be266b0447be02ffd1e3fcee820f36781c7cd8
Ron Yeung <wwwinglung@gmail.com>
2024-09-06 17:48:51 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
+import os
+from urllib.parse import urlparse
 import subprocess
+
+from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
-    code_str = """from backend.app import create_app
+    load_dotenv()
+    port = urlparse(os.getenv('REACT_APP_BACK_URL_1')).port
+    
+    code_str = f"""from backend.app import create_app
 flask_app = create_app()
-flask_app.run(debug=True, host='0.0.0.0', port=8080)"""
+flask_app.run(debug=True, host='0.0.0.0', port={port})"""
     subprocess.Popen(['python', '-c', code_str])
     subprocess.Popen(["npm", "start"], cwd='frontend')
 
