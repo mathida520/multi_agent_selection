816a133680944c517bd934dc1945a3836128b514
Ron Yeung <wwwinglung@gmail.com>
2024-09-07 22:39:02 +0800

    Add files via upload

---
+++ scripts/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PROJECT_PATH=/Users/ronglong/Documents/multi_agent_selection
+
+export PYTHONPATH=$PROJECT_PATH
+
+cd $PROJECT_PATH/backend || exit
+#source venv/bin/activate
+python run.py &
+
+FLASK_PID=$!
+
+cd $PROJECT_PATH/frontend || exit
+#npm install
+#nvm use 17
+npm start &
+
+REACT_PID=$!
+
+read -r -p "Press enter to stop"
+
+kill $FLASK_PID
+kill $REACT_PID
