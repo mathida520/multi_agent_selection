4ad8630a6a536a0d6d3a26b0a59f6bcb274f5b95
lomvxi <2692142131@qq.com>
2024-09-20 00:48:06 +0800

    Update msg_handler.py
    添加了sort_message

--- server/app/services/msg_handler.py
+++ server/app/services/msg_handler.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 
+from openai import OpenAI
+
 import random
 
 
@@ -25,8 +27,33 @@ def process_msgs(responses: List[Dict]) -> List[Dict]:
                 'color': color,
             })
     return msgs
+    
+def sort_message(modellist,message_list,question):    
+    MODEL = "gpt-4o"
+    api_key = "sk-Av21e8b884d4106abc8c43ab02aee1279504f332491Ip2FS"
+    messages=["{"+str(modellist[i])+":"+str(message_list[i])+"}" for i in range(len(modellist))]
+    content ='请判断{}内的话哪个给符合提问&'+question+'&,请根据符合程度返回回答选项的模型名称的排序,只返回模型名称即可，输出格式为【模型名1，模型名2....】,'
+    for i in range(len(message_list)): content=content+messages[i]
+    # get response from GPT-4o
+    def get_response(model, api_key, content):
+        client = OpenAI(api_key=api_key, base_url="https://api.gptsapi.net/v1")
+        completion = client.chat.completions.create(
+        model=model,
+        # temperature=0,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant. Complete the following tasks carefully!"},
+            # {"role": "system", "content": role_prompt},
+            {"role": "user", "content": content}
+            ]
+        )
+        return completion.choices[0].message.content
 
+    response = get_response(MODEL, api_key,content)
+    return response
 
 def resort_msgs(responses: List[Dict],question) -> List[Dict]:
     msgs = process_msgs(responses)
+    modellist = [msgs[i]["model"] for i in range(len(msgs))] 
+    message_list = [msgs[i]["message"] for i in range(len(msgs))] 
+    return sort_message(modellist,message_list,question)
 
